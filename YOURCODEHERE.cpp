7fe5fcdd129b47d73818be314c797e0e327d760f
PrestonGardner5081 <78433331+PrestonGardner5081@users.noreply.github.com>
2021-04-10 17:59:05 -0400

    more debugging, added vars -Preston

--- YOURCODEHERE.cpp
+++ YOURCODEHERE.cpp
@@ -15,18 +15,13 @@
 
 #include "431project.h"
 
-unsigned int getil1size(std::string configuration);
-unsigned int getdl1size(std::string configuration);
-unsigned int getl2size(std::string configuration);
-
-
 using namespace std;
 
 /*
  * Enter your PSU IDs here to select the appropriate scanning order.
  */
 #define PSU_ID_SUM (961890335 + 978478804)
-//1940369139%24=3 so we choose order 3 ie BP core FPU cache 
+//1940369139%24=3 so we choose order 3 ie BP core FPU cache
 
 /*
  * Some global variables to track heuristic progress.
@@ -37,7 +32,11 @@ using namespace std;
 unsigned int currentlyExploringDim = 0;
 bool currentDimDone = false;
 bool isDSEComplete = false;
-int myOrder [] = {12,13,14,11,2,3,4,5,6,7,8,9,10,0,1};
+int myOrder[] = {12, 13, 14, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 1};
+int currentlyExploringDim = myOrder[dimension];
+int nextValue = 0;
+int iter = 0;
+int dimension = 0;
 
 /*
  * Given a half-baked configuration containing cache properties, generate
@@ -48,7 +47,7 @@ int myOrder [] = {12,13,14,11,2,3,4,5,6,7,8,9,10,0,1};
  */
 std::string generateCacheLatencyParams(string halfBackedConfig)
 {
-int dl1assoc = extractConfigPararm(halfBackedConfig, 4);
+    int dl1assoc = extractConfigPararm(halfBackedConfig, 4);
     int il1assoc = extractConfigPararm(halfBackedConfig, 6);
     int ul2assoc = extractConfigPararm(halfBackedConfig, 9);
     unsigned int dl1size = getdl1size(halfBackedConfig);
@@ -62,77 +61,151 @@ int dl1assoc = extractConfigPararm(halfBackedConfig, 4);
     int il1overhead;
     int ul2overhead;
 
-    switch (dl1size){
-        case 2048:  dl1latency =  1;break;
-        case 4096:  dl1latency =  2;break;
-        case 8192:  dl1latency =  3;break;
-        case 16384: dl1latency =  4;break;
-        case 32768: dl1latency =  5;break;
-        case 65536: dl1latency =  6;break;
-        default:    dl1latency =  0;break;
+    switch (dl1size)
+    {
+    case 2048:
+        dl1latency = 1;
+        break;
+    case 4096:
+        dl1latency = 2;
+        break;
+    case 8192:
+        dl1latency = 3;
+        break;
+    case 16384:
+        dl1latency = 4;
+        break;
+    case 32768:
+        dl1latency = 5;
+        break;
+    case 65536:
+        dl1latency = 6;
+        break;
+    default:
+        dl1latency = 0;
+        break;
     }
-    switch (il1size){
-        case 2048:  il1latency = 1;break;
-        case 4096:  il1latency = 2;break;
-        case 8192:  il1latency = 3;break;
-        case 16384: il1latency = 4;break;
-        case 32768: il1latency = 5;break;
-        case 65536: il1latency = 6;break;
-        default:    il1latency = 0;break;
+    switch (il1size)
+    {
+    case 2048:
+        il1latency = 1;
+        break;
+    case 4096:
+        il1latency = 2;
+        break;
+    case 8192:
+        il1latency = 3;
+        break;
+    case 16384:
+        il1latency = 4;
+        break;
+    case 32768:
+        il1latency = 5;
+        break;
+    case 65536:
+        il1latency = 6;
+        break;
+    default:
+        il1latency = 0;
+        break;
     }
-    switch (ul2size){
-        case 32768:  ul2latency = 5;break;
-        case 65536:  ul2latency = 6;break;
-        case 131072: ul2latency = 7;break;
-        case 262144: ul2latency = 8;break;
-        case 524288: ul2latency = 9;break;
-        case 1048576:ul2latency = 10;break;
-        default:     ul2latency = 0;break;
+    switch (ul2size)
+    {
+    case 32768:
+        ul2latency = 5;
+        break;
+    case 65536:
+        ul2latency = 6;
+        break;
+    case 131072:
+        ul2latency = 7;
+        break;
+    case 262144:
+        ul2latency = 8;
+        break;
+    case 524288:
+        ul2latency = 9;
+        break;
+    case 1048576:
+        ul2latency = 10;
+        break;
+    default:
+        ul2latency = 0;
+        break;
     }
-    switch (dl1assoc){
-        case 0: dl1overhead=0;break;
-        case 1: dl1overhead=1;break;
-        case 2: dl1overhead=2;break;
-        case 3: dl1overhead=3;break;
+    switch (dl1assoc)
+    {
+    case 0:
+        dl1overhead = 0;
+        break;
+    case 1:
+        dl1overhead = 1;
+        break;
+    case 2:
+        dl1overhead = 2;
+        break;
+    case 3:
+        dl1overhead = 3;
+        break;
     }
-    switch (il1assoc){
-        case 0: il1overhead=0;break;
-        case 1: il1overhead=1;break;
-        case 2: il1overhead=2;break;
-        case 3: il1overhead=3;break;
+    switch (il1assoc)
+    {
+    case 0:
+        il1overhead = 0;
+        break;
+    case 1:
+        il1overhead = 1;
+        break;
+    case 2:
+        il1overhead = 2;
+        break;
+    case 3:
+        il1overhead = 3;
+        break;
     }
-    switch (ul2assoc){
-        case 0: ul2overhead=0;break;
-        case 1: ul2overhead=1;break;
-        case 2: ul2overhead=2;break;
-        case 3: ul2overhead=3;break;
-        case 4: ul2overhead=4;break;
+    switch (ul2assoc)
+    {
+    case 0:
+        ul2overhead = 0;
+        break;
+    case 1:
+        ul2overhead = 1;
+        break;
+    case 2:
+        ul2overhead = 2;
+        break;
+    case 3:
+        ul2overhead = 3;
+        break;
+    case 4:
+        ul2overhead = 4;
+        break;
     }
-    int dl1 = dl1latency + dl1overhead-1;
-    int il1 = il1latency + il1overhead-1;
-    int ul2 = ul2latency + ul2overhead-5;
-
+    int dl1 = dl1latency + dl1overhead - 1;
+    int il1 = il1latency + il1overhead - 1;
+    int ul2 = ul2latency + ul2overhead - 5;
 
     stringstream latencySettings;
-    latencySettings<<dl1<<" "<<il1<<" "<<ul2;
+    latencySettings << dl1 << " " << il1 << " " << ul2;
 
-	//
-	//YOUR CODE BEGINS HERE
-	//
+    //
+    //YOUR CODE BEGINS HERE
+    //
 
-	// This is a dumb implementation.
-	//	latencySettings = "1 1 1";
+    // This is a dumb implementation.
+    //	latencySettings = "1 1 1";
 
-	//
-	//YOUR CODE ENDS HERE
-	//
+    //
+    //YOUR CODE ENDS HERE
+    //
 
-	return latencySettings.str();
+    return latencySettings.str();
 }
 
 /*
  * Returns 1 if configuration is valid, else 0
  */
+}
 
 /*
  * Returns 1 if configuration is valid, else 0
@@ -140,56 +213,81 @@ int dl1assoc = extractConfigPararm(halfBackedConfig, 4);
 int validateConfiguration(std::string configuration)
 {
 
-	int l1blockSize;
+    int l1blockSize;
     int width;
     int ul2blockSize;
     int fpwidthString = extractConfigPararm(configuration, 11);
     int l1blockString = extractConfigPararm(configuration, 2);
     int ul2blockString = extractConfigPararm(configuration, 8);
-    switch (fpwidthString) {
-        case 0: width=1;break;
-        case 1: width=2;break;
-        case 2: width=4;break;
-        case 3: width=8;break;
+    switch (fpwidthString)
+    {
+    case 0:
+        width = 1;
+        break;
+    case 1:
+        width = 2;
+        break;
+    case 2:
+        width = 4;
+        break;
+    case 3:
+        width = 8;
+        break;
     }
-    switch (l1blockString){
-        case 0: l1blockSize=8;break;
-        case 1: l1blockSize=16;break;
-        case 2: l1blockSize=32;break;
-        case 3: l1blockSize=64;break;
+    switch (l1blockString)
+    {
+    case 0:
+        l1blockSize = 8;
+        break;
+    case 1:
+        l1blockSize = 16;
+        break;
+    case 2:
+        l1blockSize = 32;
+        break;
+    case 3:
+        l1blockSize = 64;
+        break;
     }
-    switch (ul2blockString){
-        case 0: ul2blockSize=16;break;
-        case 1: ul2blockSize=32;break;
-        case 2: ul2blockSize=64;break;
-        case 3: ul2blockSize=128;break;
+    switch (ul2blockString)
+    {
+    case 0:
+        ul2blockSize = 16;
+        break;
+    case 1:
+        ul2blockSize = 32;
+        break;
+    case 2:
+        ul2blockSize = 64;
+        break;
+    case 3:
+        ul2blockSize = 128;
+        break;
     }
 
     unsigned int dl1size = getdl1size(configuration);
     unsigned int il1size = getil1size(configuration);
     unsigned int ul2size = getl2size(configuration);
-//    string latencies = generateCacheLatencyParams(configuration);
+    //    string latencies = generateCacheLatencyParams(configuration);
 
-
-
-    if(l1blockSize >= width*8)
+    if (l1blockSize >= width * 8)
         return 0;
-    if(2*l1blockSize>ul2blockSize)
+    if (2 * l1blockSize > ul2blockSize)
         return 0;
-    if((il1size<2048)||(dl1size<2048))
+    if ((il1size < 2048) || (dl1size < 2048))
         return 0;
-    if((il1size>65536)||(dl1size>65536))
+    if ((il1size > 65536) || (dl1size > 65536))
         return 0;
-    if((ul2size<32768)||(ul2size>1048576))
+    if ((ul2size < 32768) || (ul2size > 1048576))
         return 0;
-    if(!isNumDimConfiguration(configuration))
+    if (!isNumDimConfiguration(configuration))
         return 0;
-	// The below is a necessary, but insufficient condition for validating a
-	// configuration.
-	return 1;
-	// The below is a necessary, but insufficient condition for validating a
-	// configuration.
-	return isNumDimConfiguration(configuration);
+    // The below is a necessary, but insufficient condition for validating a
+    // configuration.
+    return 1;
+    // The below is a necessary, but insufficient condition for validating a
+    // configuration.
+    return isNumDimConfiguration(configuration);
 }
 
 /*
@@ -203,92 +301,99 @@ int validateConfiguration(std::string configuration)
  * dimension until the rightmost dimension.
  */
 std::string generateNextConfigurationProposal(std::string currentconfiguration,
-											  std::string bestEXECconfiguration, std::string bestEDPconfiguration,
-											  int optimizeforEXEC, int optimizeforEDP)
+                                              std::string bestEXECconfiguration, std::string bestEDPconfiguration,
+                                              int optimizeforEXEC, int optimizeforEDP)
 {
 
-	//
-	// Some interesting variables in 431project.h include:
-	//
-	// 1. GLOB_dimensioncardinality
-	// 2. GLOB_baseline
-	// 3. NUM_DIMS
-	// 4. NUM_DIMS_DEPENDENT
-	// 5. GLOB_seen_configurations
+    //
+    // Some interesting variables in 431project.h include:
+    //
+    // 1. GLOB_dimensioncardinality
+    // 2. GLOB_baseline
+    // 3. NUM_DIMS
+    // 4. NUM_DIMS_DEPENDENT
+    // 5. GLOB_seen_configurations
 
-	std::string nextconfiguration = currentconfiguration;
-	// Continue if proposed configuration is invalid or has been seen/checked before.
-	while (!validateConfiguration(nextconfiguration) ||
-		GLOB_seen_configurations[nextconfiguration]) {
+    std::string nextconfiguration = currentconfiguration;
+    // Continue if proposed configuration is invalid or has been seen/checked before.
+    while (!validateConfiguration(nextconfiguration) ||
+           GLOB_seen_configurations[nextconfiguration])
+    {
 
-		// Check if DSE has been completed before and return current
-		// configuration.
-		if(isDSEComplete) {
-			return currentconfiguration;
-		}
+        // Check if DSE has been completed before and return current
+        // configuration.
+        if (isDSEComplete)
+        {
+            return currentconfiguration;
+        }
 
-		std::stringstream ss;
+        std::stringstream ss;
 
-		string bestConfig;
-		if (optimizeforEXEC == 1)
-			bestConfig = bestEXECconfiguration;
+        string bestConfig;
+        if (optimizeforEXEC == 1)
+            bestConfig = bestEXECconfiguration;
 
-		if (optimizeforEDP == 1)
-			bestConfig = bestEDPconfiguration;
+        if (optimizeforEDP == 1)
+            bestConfig = bestEDPconfiguration;
 
-		// Fill in the dimensions already-scanned with the already-selected best
-		// value.
-		for (int dim = 0; dim < currentlyExploringDim; ++dim) {
-			ss << extractConfigPararm(bestConfig, dim) << " ";
-		}
+        // Fill in the dimensions already-scanned with the already-selected best
+        // value.
+        for (int dim = 0; dim < currentlyExploringDim; ++dim)
+        {
+            ss << extractConfigPararm(bestConfig, dim) << " ";
+        }
 
-		// Handling for currently exploring dimension. This is a very dumb
-		// implementation.
-		nextValue += 1;
+        // Handling for currently exploring dimension. This is a very dumb
+        // implementation.
+        nextValue += 1;
 
-		if (nextValue >= GLOB_dimensioncardinality[currentlyExploringDim]) {
-			nextValue = GLOB_dimensioncardinality[currentlyExploringDim] - 1;
+        if (nextValue >= GLOB_dimensioncardinality[currentlyExploringDim])
+        {
+            nextValue = GLOB_dimensioncardinality[currentlyExploringDim] - 1;
             currentDimDone = true;
-		}
+        }
 
-		ss << nextValue << " ";
+        ss << nextValue << " ";
 
-		// Fill in remaining independent params with 0.
-		for (int dim = (currentlyExploringDim + 1);
-				dim < (NUM_DIMS - NUM_DIMS_DEPENDENT); ++dim) {
-			ss << extractConfigPararm(bestConfig, dim)<<" ";
-		}
-//        cout<<currentlyExploringDim<<" "<<ss.str();
-		//
-		// Last NUM_DIMS_DEPENDENT3 configuration parameters are not independent.
-		// They depend on one or more parameters already set. Determine the
-		// remaining parameters based on already decided independent ones.
-		//
-		string configSoFar = ss.str();
+        // Fill in remaining independent params with 0.
+        for (int dim = (currentlyExploringDim + 1);
+             dim < (NUM_DIMS - NUM_DIMS_DEPENDENT); ++dim)
+        {
+            ss << extractConfigPararm(bestConfig, dim) << " ";
+        }
+        //        cout<<currentlyExploringDim<<" "<<ss.str();
+        //
+        // Last NUM_DIMS_DEPENDENT3 configuration parameters are not independent.
+        // They depend on one or more parameters already set. Determine the
+        // remaining parameters based on already decided independent ones.
+        //
+        string configSoFar = ss.str();
 
-		// Populate this object using corresponding parameters from config.
-		ss << generateCacheLatencyParams(configSoFar);
+        // Populate this object using corresponding parameters from config.
+        ss << generateCacheLatencyParams(configSoFar);
 
-		// Configuration is ready now.
-		nextconfiguration = ss.str();
+        // Configuration is ready now.
+        nextconfiguration = ss.str();
 
-		// Make sure we start exploring next dimension in next iteration.
-		if (currentDimDone) {
-		    dimension +=1;
-		    if (dimension == (NUM_DIMS - NUM_DIMS_DEPENDENT)){
-		        dimension = 0;
-		    }
-		    nextValue = -1;
-			currentlyExploringDim = configPref[dimension];
-			currentDimDone = false;
-		}
-        iter +=1;
-		if (iter == 1000)
-		    isDSEComplete = true;
-//		    cout<<ss.str();
-		// Signal that DSE is complete after this configuration.
-		if (currentlyExploringDim == (NUM_DIMS - NUM_DIMS_DEPENDENT))
-			isDSEComplete = true;
-	}
-	return nextconfiguration;
+        // Make sure we start exploring next dimension in next iteration.
+        if (currentDimDone)
+        {
+            dimension += 1;
+            if (dimension == (NUM_DIMS - NUM_DIMS_DEPENDENT))
+            {
+                dimension = 0;
+            }
+            nextValue = -1;
+            currentlyExploringDim = myOrder[dimension];
+            currentDimDone = false;
+        }
+        iter += 1;
+        if (iter == 1000)
+            isDSEComplete = true;
+        //		    cout<<ss.str();
+        // Signal that DSE is complete after this configuration.
+        if (currentlyExploringDim == (NUM_DIMS - NUM_DIMS_DEPENDENT))
+            isDSEComplete = true;
+    }
+    return nextconfiguration;
 }
