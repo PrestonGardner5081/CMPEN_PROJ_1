a0dd7612d2c90e146e4aee475f5bd81e3c24bc23
Preston Gardner <ptg5081@psu.edu>
2021-04-10 12:13:35 -0700

    My order corrected -Preston

--- YOURCODEHERE.cpp
+++ YOURCODEHERE.cpp
@@ -31,7 +31,8 @@ using namespace std;
 unsigned int currentlyExploringDim = 0;
 bool currentDimDone = false;
 bool isDSEComplete = false;
-int myOrder = {12,13,14,11,2,3,4,5,6,7,8,9,10,15,16,17,0,1}
+int myOrder[] = {12, 13, 14, 11, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1};
+//int myOrder = {12,13,14,11,2,3,4,5,6,7,8,9,10,15,16,17,0,1}
 
 /*
  * Given a half-baked configuration containing cache properties, generate
