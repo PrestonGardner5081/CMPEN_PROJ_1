ee31df4d9c5bbf9b81312ddcec0803cf13ec52aa
PrestonGardner5081 <78433331+PrestonGardner5081@users.noreply.github.com>
2021-04-11 20:32:17 -0400

    bug fix

--- YOURCODEHERE.cpp
+++ YOURCODEHERE.cpp
@@ -36,7 +36,7 @@ using namespace std;
  */
 bool currentDimDone = false;
 bool isDSEComplete = false;
-int myOrder[] = {12, 13, 14, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15,16,17, 0, 1};
+int myOrder[] = {12, 13, 14, 11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0, 1};
 vector<int> explored_values;
 vector<int> best_val;
 vector<int> explore;
